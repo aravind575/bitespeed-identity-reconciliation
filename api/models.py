726e2cbebe4e75b2c87b98a5aa7fd0592dcbdd62
aravind.sridhar <aravind.sridhar@conxai.com>
2023-06-26 15:30:02 +0530

    api url routing; "Contact" model and serialzier

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    phoneNumber = models.CharField(max_length=10, null=True)
+    email = models.EmailField(max_length=254, null=True)
+    linkedId = models.BigIntegerField(null=True)
+    linkPrecedence = models.CharField(max_length=10, choices=[("secondary", "Secondary"), ("primary", "Primary")])
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    deletedAt = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f"Contact {self.id}"
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .serializers import ContactSerializer
+from .models import Contact
+
+
+class IdentityView(APIView):
+    permission_classes = [AllowAny]
+    # authentication_classes = []
+
+    def post(self, request):
+        ...
